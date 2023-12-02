246aff0f93bd01e81a09a0d9004ad18c5e6ee6e6
MurariuMarius <mam.murariu@gmail.com>
2023-12-02 10:55:27 +0200

    Initialize firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSCRf3XGEJ71M3U0dK2ljdng-tPHok2Mg",
+  authDomain: "memories-8f4d0.firebaseapp.com",
+  projectId: "memories-8f4d0",
+  storageBucket: "memories-8f4d0.appspot.com",
+  messagingSenderId: "278299107663",
+  appId: "1:278299107663:web:9d0f9c9013c68c59d5253a"
+};
+
+const app = initializeApp(firebaseConfig);
