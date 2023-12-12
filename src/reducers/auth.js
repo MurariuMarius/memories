c77d3c22aa8b3fa302f57e26850af62ceac6a5c8
MurariuMarius <mam.murariu@gmail.com>
2023-12-12 13:16:41 +0200

    Handle user logout with firebase

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,4 +1,6 @@
+import { signOut } from 'firebase/auth';
 import * as actionType from '../constants/actionTypes';
+import { authService } from '../firebase/config';
 
 const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
@@ -8,7 +10,7 @@ const authReducer = (state = { authData: null }, action) => {
       return { ...state, authData: action.data, loading: false, errors: null };
     case actionType.LOGOUT:
       localStorage.clear();
-
+      signOut(authService);
       return { ...state, authData: null, loading: false, errors: null };
     default:
       return state;
