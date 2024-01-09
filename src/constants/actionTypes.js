3082c6dde97205ba2ea9d1a1ec45191bdd5b53e7
darius.rotari <darius.rotari@student.upt.ro>
2024-01-09 15:07:59 +0200

    adding search functionality

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,13 +2,17 @@ export const CREATE = 'CREATE';
 export const UPDATE = 'UPDATE';
 export const DELETE = 'DELETE';
 export const FETCH_ALL = 'FETCH_ALL';
+
+export const FETCH_POST = 'FETCH_POST';
 export const FETCH_BY_SEARCH = 'FETCH_BY_SEARCH';
 export const FETCH_POST = 'FETCH_POST';
 export const COMMENT = 'COMMENT';
 export const END_LOADING = 'END_LOADING';
 export const START_LOADING = 'START_LOADING';
-export const UPDATE_POST_FORM = 'UPDATE_POST_FORM'
+export const UPDATE_POST_FORM = 'UPDATE_POST_FORM'export const COMMENT = 'COMMENT';
+export const END_LOADING = 'END_LOADING';
+export const START_LOADING = 'START_LOADING';
+export const FETCH_BY_CREATOR = 'FETCH_BY_CREATOR';
+export const FETCH_BY_SEARCH = 'FETCH_BY_SEARCH';
 export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
-
-export const FETCH_BY_CREATOR = 'FETCH_BY_CREATOR';
